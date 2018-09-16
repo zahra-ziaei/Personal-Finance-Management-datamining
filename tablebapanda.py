34b9b3fccc25e9946f19bebebef5891c5cc2523a
zahra-ziaei <41682842+zahra-ziaei@users.noreply.github.com>
2018-09-16 10:41:54 +0430

    Update tablebapanda.py

--- tablebapanda.py
+++ tablebapanda.py
@@ -1,8 +1,30 @@
 
+"""
+# save thae data as it represented  
 import pandas as pd
 
-tables = pd.read_html('http://www.fipiran.com/Fund/MFComparing/1',header=0)
+tables = pd.read_html('http://www.fipiran.com/Market/LupBourse',header=0)
 
 print(tables[0])
 
-tables[0].to_csv('pandatab.csv')
+tables[0].to_csv('pandatabburs.csv')
+
+"""
+
+# save thae data as decreasingly
+
+import pandas as pd
+
+tables = pd.read_html('http://www.fipiran.com/Fund/MFComparing/3',header=0)
+
+
+data=tables[0]
+
+
+data.columns.values
+
+datas=data.sort_values(by=['بازدهی ماه(%)'],ascending=False)
+
+
+datas.to_csv('pandatabsandogh.csv')
+
