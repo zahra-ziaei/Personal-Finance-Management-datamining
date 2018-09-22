dac718a61f6101f23b9ff95af945aa0bd6ac1053
zahra-ziaei <41682842+zahra-ziaei@users.noreply.github.com>
2018-09-22 16:08:36 +0330

    Update tablebapanda.py

--- tablebapanda.py
+++ tablebapanda.py
@@ -17,14 +17,31 @@ import pandas as pd
 
 tables = pd.read_html('http://www.fipiran.com/Fund/MFComparing/3',header=0)
 
-
 data=tables[0]
 
 
-data.columns.values
+#data.dtypes
 
-datas=data.sort_values(by=['بازدهی ماه(%)'],ascending=False)
+#data.columns.values
+
+"to remove M character and ,"
+datam=data.replace('M','',regex=True)
+
+datam=datam.replace(',','',regex=True)
+
+#datam.dtypes
+
+datam=datam[1:]
+#this columns value is objective and I change it to int
+
+datam[  'a']=datam[  'خالص ارزش دارایی صندوق(ریال)'].astype('int64')*1000000
+datam[  'ارزش خالص هر واحد سرمایه گذاری (NAV)(ریال)'].astype('int64')
+
+
+datam['تعداد واحدهای سرمایه گذاری']=datam[ 'a']/datam['ارزش خالص هر واحد سرمایه گذاری (NAV)(ریال)']
+
+
+datas=datam.sort_values(by=['بازدهی ماه(%)'],ascending=False)
 
 
 datas.to_csv('pandatabsandogh.csv')
-
